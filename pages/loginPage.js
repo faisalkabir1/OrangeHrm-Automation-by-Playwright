9f766f8115f33f6bdc8f36fce92fb6f25d5f5769
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-08-15 12:21:53 +0600

    Create loginPage.js

---
+++ pages/loginPage.js
@@ -0,0 +1,21 @@
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = 'input[name="username"]';
+    this.passwordInput = 'input[name="password"]';
+    this.loginButton = 'button[type="submit"]';
+    this.dashboardHeader = 'h6';
+  }
+
+  async gotoLoginPage() {
+    await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  }
+
+  async login(username, password) {
+    await this.page.fill(this.usernameInput, username);
+    await this.page.fill(this.passwordInput, password);
+    await this.page.click(this.loginButton);
+  }
+}
+
+module.exports = { LoginPage };
