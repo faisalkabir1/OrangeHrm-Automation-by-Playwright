bdfcd8f6793b11681117798c006daa5b7da2429c
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-08-15 12:22:17 +0600

    update load time config

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
   expect: {
-    timeout: 10000,
+    timeout: 20000,
   },
 
   use: {
